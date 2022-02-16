218a5533a889081d0e93189b0cb51991ac48303d
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-02-16 15:02:25 +0200

    Update gulpfile.js
    First working version.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,94 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+'use strict';
+
+
+const { src, dest, watch, series, parallel } = require('gulp');
+
+const sass        = require('gulp-sass')(require('sass'));
+const concat      = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const minify      = require('gulp-terser');
+const imagemin    = require('gulp-imagemin');
+const del         = require('del');
+const fileinclude = require('gulp-file-include');
+
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "src/",
+    },
+  });
+  watch('src/*.html').on('change', browserSync.reload);
 }
 
-exports.default = defaultTask;
+function clearDist() {
+  return del('dist/');
+}
+
+function buildhtml() {
+  return src('src/layouts/**/*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest('src/'));
+}
+
+function styles() {
+  return src('src/sass/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(concat('style.min.css'))
+    .pipe(dest('src/css/'))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return src('src/js/main.js')
+    .pipe(concat('main.min.js'))
+    .pipe(minify())
+    .pipe(dest('src/js'))
+    .pipe(browserSync.stream());
+}
+
+function images() {
+  return src('src/images/**/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+          plugins: [
+              {removeViewBox: true},
+              {cleanupIDs: false}
+          ]
+      })
+  ]))
+    .pipe(dest('dist/images/'));
+}
+
+function build() {
+  return src([
+    'src/*.html',
+    'src/css/style.min.css',
+    'src/js/main.min.js',
+    'src/fonts/**/*',
+  ], {base: 'src/'})
+    .pipe(dest('dist/'));
+}
+
+function watcher() {
+  watch(['src/sass/**/*.scss'], styles);
+  watch(['src/**/*.html', '!src/*.html'], buildhtml);
+  watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts);
+}
+
+exports.browsersync = browsersync;
+exports.clearDist = clearDist;
+exports.buildhtml = buildhtml;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.watcher = watcher;
+
+exports.build = series(clearDist, images, build);
+exports.default = parallel(buildhtml, styles, scripts, browsersync, watcher);
