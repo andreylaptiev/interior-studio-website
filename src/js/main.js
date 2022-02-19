22e0901a990f220c7082da9776bc98dfab40a28c
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-02-19 20:16:31 +0200

    Add script to make header sticky

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,14 @@ burgerMenuBtn.addEventListener('click', () => {
     document.body.classList.toggle('_lock');
   }
 });
+
+
+// sticky header
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 1) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
